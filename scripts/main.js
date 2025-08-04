f543889d96774c3068524907972a3d3390b92873
jensert <jensert@local.com>
2025-08-04 17:39:57 +0200

    moved all functions to TodoCard class

--- scripts/main.js
+++ scripts/main.js
@@ -9,38 +9,30 @@ todoInput.addEventListener("keydown", (event) => {
     (event.key === "Enter") ? addNewTodo(todoInput.value) : null;
 })
 
-console.log(todoCardHolder);
-
-function toggleTodoCardDone(todoCard) {
-    if (todoCard.textArea.className === "todoCardDetails") {
-        todoCard.textArea.className = "todoCardDetailsDone";
-    } else {
-        todoCard.textArea.className = "todoCardDetails";
-    }
-}
-
 class TodoCard {
     constructor(text) {
         this.div = document.createElement("div")
-        this.div.textContent = text
         this.div.className = "todoCard"
 
         // Add text area to item div
         this.textArea = document.createElement("textarea")
         this.textArea.className = "todoCardDetails"
+        this.textArea.textContent = text;
         this.div.appendChild(this.textArea);
 
         // Add 'Done' button
         this.doneButton = document.createElement("button")
         this.doneButton.textContent = "Mark as done"
+        this.doneButton.className = "confirmButton"
         this.doneButton.addEventListener("click", () => {
-            toggleTodoCardDone(this)
+            this.toggleDone()
         })
         this.div.appendChild(this.doneButton)
 
         // Add 'remove' button
         this.removeButton = document.createElement("button")
         this.removeButton.textContent = "Remove"
+        this.removeButton.className = "confirmButton"
         this.removeButton.addEventListener("click", () => {
             this.destruct()
         })
@@ -49,6 +41,14 @@ class TodoCard {
 
     destruct() {
         this.div.remove();
+    }
+
+    toggleDone() {
+        if (this.textArea.className === "todoCardDetails") {
+            this.textArea.className = "todoCardDetailsDone";
+        } else {
+            this.textArea.className = "todoCardDetails";
+        }
     }
 }
 
